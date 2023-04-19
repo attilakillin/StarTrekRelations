3c3272e6aa0c476735d56573a805e65c008fa41b
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2023-04-19 17:05:52 +0200

    Fix build and dependency version issues

--- app/src/main/java/hu/attilakillin/startrekrelations/persistence/CharacterDao.kt
+++ app/src/main/java/hu/attilakillin/startrekrelations/persistence/CharacterDao.kt
@@ -2,6 +2,7 @@ package hu.attilakillin.startrekrelations.persistence
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import hu.attilakillin.startrekrelations.model.Character
 import hu.attilakillin.startrekrelations.model.CharacterWithRelations
 
@@ -10,6 +11,7 @@ interface CharacterDao {
     @Query("SELECT * FROM character")
     fun loadAllCharacters(): List<Character>
 
+    @Transaction
     @Query("SELECT * FROM character WHERE character.uid = :uid")
     fun loadCharacterDetails(uid: String): CharacterWithRelations
 }
--- app/src/main/java/hu/attilakillin/startrekrelations/persistence/CharacterDatabase.kt
+++ app/src/main/java/hu/attilakillin/startrekrelations/persistence/CharacterDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import hu.attilakillin.startrekrelations.model.Character
 import hu.attilakillin.startrekrelations.model.Relation
 
-@Database(entities = [Character::class, Relation::class], version = 1)
+@Database(entities = [Character::class, Relation::class], version = 1, exportSchema = false)
 abstract class CharacterDatabase : RoomDatabase() {
     abstract fun characterDao(): CharacterDao
 
