e68e9e0a849aa6b422b419741f42b5b439847e44
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2023-04-19 12:42:04 +0200

    Initialize project

---
+++ app/src/main/java/hu/attilakillin/startrekrelations/MainActivity.kt
@@ -0,0 +1,46 @@
+package hu.attilakillin.startrekrelations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import hu.attilakillin.startrekrelations.ui.theme.StarTrekRelationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StarTrekRelationsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StarTrekRelationsTheme {
+        Greeting("Android")
+    }
+}
